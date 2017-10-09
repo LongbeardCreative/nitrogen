8bdbbb9d8ea873e3b21d5092f0e28b9217ac673a
Evan Hennessy <hennessyevan@users.noreply.github.com>
2017-10-09 13:18:52 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name:  Profectus
+Plugin URI:   https://www.longbeard.com/profectus-report
+Description:  Automates various LB specific tasks. Do not delete.
+Version:      0.0.1
+Author:       Evan Hennessy
+Author URI:   https://www.hennessyevan.com/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
