50eb372059fa0459c543caf5aaa8dfae70312908
Evan Hennessy <hennessyevan@users.noreply.github.com>
2017-10-10 08:59:28 -0400

    Setup Plugin Page

--- profectus.php
+++ profectus.php
@@ -9,3 +9,15 @@ Author URI:   https://www.hennessyevan.com/
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
+
+function profectus_init() {
+    add_sub_menu_page(
+        'toplevel_page_ct_dashboard_page',
+        'Profectus Settings',
+        'Profectus',
+        'manage_options',
+        'profectus',
+        'profectus_settings_callback'
+    );
+}
+add_action( 'admin_menu', 'profectus_init' );
